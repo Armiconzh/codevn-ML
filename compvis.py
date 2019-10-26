097fd78e71887889f0caf9932d5d692f01d8f9ac
ARMICO <armicoooonzh19@gmail.com>
2019-10-26 21:09:54 +0700

    codeVn

---
+++ compvis.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+mnist = tf.keras.dataset
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images=training_images.reshape(60000, 28, 28, 1)
+training_images=training_images / 255.0
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images=test_images / 255.0
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+])
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+model.summary()
+model.fit(training_images, training_labels, epochs=5)
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print ('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy*100)
